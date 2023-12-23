7f35b24a8e4718f5550896f1658668e13bdb2b32
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-12-23 22:31:10 +0100

    first commit

---
+++ test.h
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+#define hello_world "hello world"
+#define a(b) b + 2
+char test(char* test_str);
+double test2(int number);
+void test3(char c);
+int test4(double arg);
+struct hello {
+    int a;
+    double b;
+};
+typedef int test_t;
+int testfunction(){
+    return 1;
+}
